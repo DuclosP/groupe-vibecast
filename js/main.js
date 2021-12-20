725df7f273f441ba6fc16aafb3eeca10f6981b5a
duclos pierre <duclospi@gmail.com>
2021-12-20 16:14:59 +0100

    un peut de js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
 console.log('Coucou les pipous ♥ :D');
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal' ,
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
