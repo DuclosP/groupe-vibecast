2342a3f285869f7a8cbec10641048443ead65ee4
duclos pierre <duclospi@gmail.com>
2021-12-29 06:34:03 +0100

    css et js  de la section 8 ( FAQ )

--- js/main.js
+++ js/main.js
@@ -1,19 +1,31 @@
 console.log('Coucou les pipous ♥ :D ');
-
+//section5 Le swiper
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    direction: 'horizontal' ,
-    loop: true,
+  // Optional parameters
+  direction: 'horizontal' ,
+  loop: true,
   
-    // If we need pagination
-    pagination: {
-      el: '.swiper-pagination',
-      clickable:"true",
-    },
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable:"true",
+  },
   
-    // Navigation arrows
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+});
+
+//section8 les plus
+let question = document.querySelectorAll(".questions-box");
+let defilQuestion = document.querySelectorAll(".defil-question");
+let btnPlus = document.querySelectorAll(".plus");
+
+for(let i=0; i<btnPlus.length; i++){
+  question[i].addEventListener("click", () => {
+    defilQuestion[i].classList.toggle("open");
+    btnPlus[i].classList.toggle("moin");
+  })
+}
